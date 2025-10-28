75d7595e777bd9ed09aa62e7bc482c6351014d7f
Sébastien Nobour <sebastien@devedanos.com>
2025-10-28 18:17:05 +0100

    test(home): add tests for app layout

---
+++ src/__tests__/layout.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import Layout from "../app/layout";
+
+describe("Feature: Layout", () => {
+  const renderLayout = () => render(<Layout>children</Layout>)
+
+  describe("Given the Layout component is rendered", () => {
+    describe.todo("When the page loads", () => {
+      test.todo("Then its content is present");
+    });
+
+    describe("When user clicks on the Ninja logo", () => {
+      test.fails("Then user is redirected to the home page `/`", async () => {
+        renderLayout()
+        expect(screen.getByRole('link', { name: 'fonts ninja logo - return to home page' }))
+      });
+    });
+
+    describe("When user clicks on the Switch Theme button", () => {
+      test.todo("Then the color theme changes from light to dark");
+      test.todo("Then the color theme changes from dark to light");
+    });
+  });
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header></header>
         {children}
       </body>
     </html>
